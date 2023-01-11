5d9f7e1da0ec9530124a911d20165bb26e04d7fb
Thomas Prévost <thomasprevost85@gmail.com>
2023-01-11 09:37:18 +0100

    begin rework on frontend

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/static")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
